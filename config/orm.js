bb8b6a149ea2b7656e988154fdd6cfe88f461623
Marie Nabors <marienabors@thefirstacademy.org>
2018-03-01 01:27:29 -0500

    debugging orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,29 +1,29 @@
 var connection = require("./connection.js");
 
 var orm = {
-  all: function(tableInput, cb){
-    connection.query("SELECT * FROM " +tableInput+";", function(err, result) {
-      if (err) {
-        throw err;
+all: function(tableInput, cb){
+connection.query("SELECT * FROM " +tableInput+";", function(err, result) {
+  if (err) {
+  throw err;
       }
-      cb(result);
+cb(result);
     });
   },
 
-  update: function(tableInput, condition, cb){
-    connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function(err, result) {
-      if (err) {
-        throw err;
+update: function(tableInput, condition, cb){
+ connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function(err, result) {
+ if (err) {
+  throw err;
       }
-      cb(result);
+cb(result);
     });
   },
   create: function(tableInput, val, cb){
-    connection.query('INSERT INTO '+tableInput+" VALUES ('" + val +"');", function(err, result) {
-      if (err) {
-        throw err;
-      }
-      cb(result);
+  connection.query('INSERT INTO '+tableInput+"(burger_name) VALUES ('" + val +"');", function(err, result) {
+   if (err) {
+    throw err;
+     }
+  cb(result);
     });
   },
 }
